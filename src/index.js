c80e43751fe6cab0829926928453d9a39da7112a
Frankie Bagnardi <f.bagnardi@gmail.com>
2015-09-11 23:53:42 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import {Children} from 'react';
+import {inspect} from 'util';
+import shallowDiff from 'shallow-diff';
+
+const ANY = jsxEquals.ANY = function JsxEqualsAny(){};
+
+export default function jsxEquals(original, template, opts={}, state={path: ''}){
+  function makeError(msg, ...values){
+    var res = msg.replace(/%(\d+)/g, (m, index) => {
+      return inspect(values[index]);
+    });
+
+    res = `jsxEquals at path ${inspect(state.path)}: ${res}`;
+
+    if (opts.noThrow) return new Error(res);
+    else throw new Error(res);
+  }
+
+  if (typeof original === 'string' && typeof template === 'string') {
+    return original === template;
+  }
+
+  opts.allowExtraProps = opts.allowExtraProps != null ? opts.allowExtraProps : true;
+  const {allowExtraProps} = opts;
+
+  // compare type
+  if (template.type !== ANY && original.type !== template.type) {
+    return makeError('Types %0 and %1 are not equal', original.type, template.type);
+  }
+
+  // compare propsA
+  const cleanProps = (props) => Object.keys(props).reduce((acc, key) => {
+    if (key !== 'children' && key[0] !== '_') acc[key] = props[key];
+    return acc;
+  }, {});
+
+  var propDiff = shallowDiff(cleanProps(template.props), cleanProps(original.props));
+  if (allowExtraProps) {
+    propDiff.added = [];
+  }
+  if (propDiff.updated.length && propDiff.added.length || propDiff.updated.length || propDiff.deleted.length) {
+    return makeError('Props don\'t match %0', {
+      added: propDiff.deleted,
+      updated: propDiff.updated,
+      deleted: propDiff.added,
+    });
+  }
+
+
+  const childrenToArray = (input) => {var out = []; Children.forEach(input, (x) => out.push(x)); return out};
+  var childrenA = childrenToArray(original.props.children);
+  var childrenB = childrenToArray(template.props.children);
+  for (let i=0; i<childrenA.length; i++) {
+    let childA = childrenA[i];
+    let childB = childrenB[i];
+
+    // FIXME: handle ANY here somehow
+    const type = childA.type && childA.type
+      ? childA.type.name ? childA.type.name : childA.type
+      : `[[${typeof childA}]]`;
+    let res = jsxEquals(childA, childB, opts, {
+      path: `${state.path}.${type}`
+    });
+    if (res instanceof Error) return res;
+  }
+
+  return true;
+}
+
+
---
+++ test/index.test.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import jsxEquals from '../src/';
+import {expect} from 'chai';
+
+const jsxNotEquals = (...args) => {
+  expect(() => jsxEquals(...args)).to.throw();
+}
+
+describe('jsxEquals', () => {
+  it('works for truthy case', () => {
+    jsxEquals(<div />, <div />);
+    jsxEquals(<div id="foo" />, <div />);
+    jsxEquals(
+      <div className="foo" />,
+      <div className="foo" />
+    );
+    jsxEquals(
+      <div className="foo"><span>foo</span></div>,
+      <div className="foo"><span>foo</span></div>
+    );
+
+    var C1 = function C1(){};
+    var C2 = function C2(){};
+    jsxEquals(
+      <div>
+        Kitchen {'sink'}
+        <span data-foo="bar">
+          <table>
+            <tr><td /></tr>
+            <tr><td><C2 /></td></tr>
+            <tr><td /></tr>
+          </table>
+        </span>
+        <C1>
+          <input value="foo" onChange={() => {}} />
+        </C1>
+      </div>,
+      <div>
+        Kitchen {'sink'}
+        <span data-foo="bar">
+          <table>
+            <tr><td /></tr>
+            <tr><td><C2 /></td></tr>
+            <tr><td /></tr>
+          </table>
+        </span>
+        <C1>
+          <input value="foo" />
+        </C1>
+      </div>
+    );
+  });
+
+  it('fails for falsy cases', () => {
+    jsxNotEquals(<div />, <div id="x" />);
+    jsxNotEquals(
+      <div className="foo" />,
+      <div className="bar" />
+    );
+    jsxNotEquals(
+      <div className="foo"><span>foo</span></div>,
+      <span className="foo"><span>foo</span></span>
+    );
+    jsxNotEquals(
+      <div className="foo"><span>foo</span></div>,
+      <div className="foo"><p>foo</p></div>
+    );
+
+    var C1 = function C1(){};
+    var C2 = function C2(){};
+    // div > span > table > tr:nth(2) > td > C2 changed to C1
+    jsxNotEquals(
+      <div>
+        Kitchen {'sink'}
+        <span data-foo="bar">
+          <table>
+            <tr><td /></tr>
+            <tr><td><C2 /></td></tr>
+            <tr><td /></tr>
+          </table>
+        </span>
+        <C1>
+          <input value="foo" onChange={() => {}} />
+        </C1>
+      </div>,
+      <div>
+        Kitchen {'sink'}
+        <span data-foo="bar">
+          <table>
+            <tr><td /></tr>
+            <tr><td><C1 /></td></tr>
+            <tr><td /></tr>
+          </table>
+        </span>
+        <C1>
+          <input value="foo" />
+        </C1>
+      </div>
+    );
+  });
+
+  it('works with ANY', () => {
+    jsxEquals(<div />, <jsxEquals.ANY />);
+  });
+
+});
+
+
